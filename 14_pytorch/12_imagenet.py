1f7d4235cbe046484f39cf2a8e945a428285c540
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2025-06-02 19:10:27 +0900

    Use TSUBAME imagenet.

--- 14_pytorch/12_imagenet.py
+++ 14_pytorch/12_imagenet.py
@@ -65,14 +65,14 @@ def main():
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    train_dataset = datasets.ImageFolder('/gs/bs/tga-hpc-lecture/imagenet/train',
+    train_dataset = datasets.ImageFolder('/gs/fs/datasets/academic/ILSVRC2012_img_train',
                                          transforms.Compose([
                                              transforms.RandomResizedCrop(224),
                                              transforms.RandomHorizontalFlip(),
                                              transforms.ToTensor(),
                                              normalize
                                          ]))
-    val_dataset = datasets.ImageFolder('/gs/bs/tga-hpc-lecture/imagenet/val',
+    val_dataset = datasets.ImageFolder('/gs/fs/datasets/academic/ILSVRC2012_img_val',
                                          transforms.Compose([
                                              transforms.Resize(256),
                                              transforms.CenterCrop(224),
